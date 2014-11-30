fa3c6ccbb6e6c991786baa20ad6264ec117b9707
DanielWebster <gaminglogos@gmail.com>
2014-11-29 20:12:34 -0500

    test = code necessary for rsa encryption/decryption

--- Chat/test.py
+++ Chat/test.py
@@ -1,7 +1,25 @@
-from Crypto.Hash import *
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(1024)
+
+binPrivKey = key.exportKey('DER')
+binPubKey =  key.publickey().exportKey('DER')
+
+privKeyObj = RSA.importKey(binPrivKey)
+pubKeyObj =  RSA.importKey(binPubKey)
+
+msg = "attack at dawn"
+emsg = pubKeyObj.encrypt(msg, 'x')[0]
+dmsg = privKeyObj.decrypt(emsg)
+emsg2 = privKeyObj.decrypt(msg)
+dmsg2 = pubKeyObj.encrypt(emsg2, 'x')[0]
 
 
-hash = SHA512.new()
-hash.update('message')
-print hash.digest()
+print "private key: " + binPrivKey
+print "public key: " + binPubKey
 
+print msg
+print emsg
+print dmsg
+print "emsg2: " + emsg2
+print "dmsg2: " + dmsg2

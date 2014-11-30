211a4eae55a2951b476527c1f7b6873554a515b2
DanielWebster <gaminglogos@gmail.com>
2014-11-29 21:08:43 -0500

    Removed ACM Stuff.

--- Chat/test.py
+++ Chat/test.py
@@ -1,9 +1,11 @@
 from Crypto.PublicKey import RSA
 
+f = open('PrivateKey', 'w')
+
 key = RSA.generate(1024)
 
-binPrivKey = key.exportKey('DER')
-binPubKey =  key.publickey().exportKey('DER')
+binPrivKey = key.exportKey('PEM')
+binPubKey =  key.publickey().exportKey('PEM')
 
 privKeyObj = RSA.importKey(binPrivKey)
 pubKeyObj =  RSA.importKey(binPubKey)
@@ -13,7 +15,11 @@ emsg = pubKeyObj.encrypt(msg, 'x')[0]
 dmsg = privKeyObj.decrypt(emsg)
 emsg2 = privKeyObj.decrypt(msg)
 dmsg2 = pubKeyObj.encrypt(emsg2, 'x')[0]
-
+f.write(binPrivKey)
+f.close()
+f = open('PublicKey', 'w')
+f.write(binPubKey)
+f.close()
 
 print "private key: " + binPrivKey
 print "public key: " + binPubKey
